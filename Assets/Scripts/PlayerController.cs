546170db30eeb630b9b73f8ddc71ac6bfbbe3f54
Etienne <etiennepenault1997@gmail.com>
2022-10-22 18:57:21 +0200

    Overflow done

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,27 +5,46 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public SpriteRenderer spriteRenderer;
+
     public Sprite[] sprites;
 
+    public GameObject doodle;
+
+    public GameObject doodleOverflow;
+
     private Rigidbody2D rb;
+
     private bool isFalling;
+
     private bool isBended;
+
     private float bendedTimer;
+
     private bool isAttacking;
+
     private float attackTimer;
+
     private int currentSpriteIndex;
+
     private int lastSide;
 
     // two types of height when player jump
     private readonly float defaultJumpHeight = 5f;
+
     private readonly float springJumpHeight = 10f;
+
     private readonly int LEFT_SIDE_WITH_BENDED_LEG = 0;
+
     private readonly int LEFT_SIDE = 1;
+
     private readonly int HEAD_UP_WITH_FOOT = 2;
+
     private readonly int HEAD_UP_WITHOUT_FOOT = 3;
+
     private readonly int RIGHT_SIDE_WITH_BENDED_LEG = 4;
+
     private readonly int RIGHT_SIDE = 5;
-    
+
     // Start is called before the first frame update
     void Start()
     {
@@ -35,6 +54,14 @@ public class PlayerController : MonoBehaviour
 
         rb = GetComponent<Rigidbody2D>();
         isFalling = true;
+
+        // Init other doodle for the OverFlow
+        if (doodle.name == "lik-right")
+        {
+            doodleOverflow = GameObject.Instantiate(doodle);
+            doodleOverflow.transform.position = new Vector3(3f, 3f, 3f);
+            doodleOverflow.SetActive(false);
+        }
     }
 
     // Update is called once per frame
@@ -47,9 +74,9 @@ public class PlayerController : MonoBehaviour
             movement += Vector3.left;
             lastSide = LEFT_SIDE;
             if (isBended)
-                changeSprite(LEFT_SIDE_WITH_BENDED_LEG);
+                ChangeSprite(LEFT_SIDE_WITH_BENDED_LEG);
             else
-                changeSprite(LEFT_SIDE);
+                ChangeSprite(LEFT_SIDE);
         }
 
         if (Input.GetKey(KeyCode.RightArrow) == true)
@@ -57,18 +84,18 @@ public class PlayerController : MonoBehaviour
             movement += Vector3.right;
             lastSide = RIGHT_SIDE;
             if (isBended)
-                changeSprite(RIGHT_SIDE_WITH_BENDED_LEG);
+                ChangeSprite(RIGHT_SIDE_WITH_BENDED_LEG);
             else
-                changeSprite(RIGHT_SIDE);
+                ChangeSprite(RIGHT_SIDE);
         }
 
         if (Input.GetKey(KeyCode.UpArrow) == true)
         {
             if (isBended)
-                changeSprite(HEAD_UP_WITH_FOOT);
+                ChangeSprite(HEAD_UP_WITH_FOOT);
             else
-                changeSprite(HEAD_UP_WITHOUT_FOOT);
-            shoot();
+                ChangeSprite(HEAD_UP_WITHOUT_FOOT);
+            Shoot();
         }
 
         if (isBended && bendedTimer >= 0.25f)
@@ -76,22 +103,25 @@ public class PlayerController : MonoBehaviour
             isBended = false;
             bendedTimer = 0f;
             if (isAttacking)
-                changeSprite(HEAD_UP_WITHOUT_FOOT);
+                ChangeSprite(HEAD_UP_WITHOUT_FOOT);
             else
-                changeSprite(lastSide);
+                ChangeSprite(lastSide);
         }
-        
+
         if (isAttacking && attackTimer >= 0.25f)
         {
             isAttacking = false;
             attackTimer = 0f;
-            changeSprite(lastSide);
+            ChangeSprite (lastSide);
         }
 
         bendedTimer += Time.deltaTime;
         attackTimer += Time.deltaTime;
         transform.position += movement * 2.5f * Time.deltaTime;
         isFalling = rb.velocity.y <= 0;
+
+        // Handling Overflow only for the init one
+        if (doodle.name == "lik-right") OverflowHandle();
     }
 
     // function which check the collision of the player with other gameobject
@@ -101,10 +131,14 @@ public class PlayerController : MonoBehaviour
         if (isFalling)
         {
             // jump only when tiles are green, blue or white
-            if ( (other.gameObject.CompareTag("green_tile")) || (other.gameObject.CompareTag("blue_tile")) || (other.gameObject.CompareTag("white_tile")))
+            if (
+                (other.gameObject.CompareTag("green_tile")) ||
+                (other.gameObject.CompareTag("blue_tile")) ||
+                (other.gameObject.CompareTag("white_tile"))
+            )
             {
-                jump(defaultJumpHeight);
-                changeSprite(currentSpriteIndex - 1);
+                Jump (defaultJumpHeight);
+                ChangeSprite(currentSpriteIndex - 1);
             }
             // else destroy tiles
             // ...
@@ -112,7 +146,7 @@ public class PlayerController : MonoBehaviour
     }
 
     // player jump function
-    void jump(float height)
+    void Jump(float height)
     {
         isBended = true;
         bendedTimer = 0f;
@@ -122,7 +156,7 @@ public class PlayerController : MonoBehaviour
     }
 
     // player shoot function
-    void shoot()
+    void Shoot()
     {
         isAttacking = true;
         attackTimer = 0f;
@@ -136,10 +170,50 @@ public class PlayerController : MonoBehaviour
      * 4 -> right with leg bended, 5 -> right
      * 
      */
-    void changeSprite(int spriteIndex)
+    void ChangeSprite(int spriteIndex)
     {
-        Debug.Log(spriteIndex);
+        Debug.Log (spriteIndex);
         currentSpriteIndex = spriteIndex;
         spriteRenderer.sprite = sprites[spriteIndex];
+    }
+
+    void OverflowHandle()
+    {
+        // Is doodle cutted ?
+        bool over =
+            (
+            doodleOverflow.transform.position.x -
+            spriteRenderer.bounds.size.x / 2
+            ) >
+            -1.55f &&
+            (
+            doodleOverflow.transform.position.x +
+            spriteRenderer.bounds.size.x / 2
+            ) <
+            1.55f;
+        bool cutLeft =
+            (transform.position.x - spriteRenderer.bounds.size.x / 2) < -1.55f;
+        bool cutRight =
+            (transform.position.x + spriteRenderer.bounds.size.x / 2) > 1.55f;
+
+        doodleOverflow.SetActive(true);
+
+        if (over)
+        {
+            Vector3 tmp = transform.position;
+            transform.position = doodleOverflow.transform.position;
+            doodleOverflow.transform.position = tmp;
+            doodleOverflow.SetActive(false);
+        }
+        else if (cutLeft)
+        {
+            doodleOverflow.transform.position =
+                transform.position + new Vector3(3.1f, 0f, 0f);
+        }
+        else if (cutRight)
+        {
+            doodleOverflow.transform.position =
+                transform.position - new Vector3(3.1f, 0f, 0f);
+        }
     }
 }
